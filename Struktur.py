2e113662f31bbac6a395cd1238d5d3c0fb060a5d
才到天中万国明 <72687976+Xiangyu-Jin@users.noreply.github.com>
2022-04-11 15:21:29 +0200

    Add files via upload

---
+++ Struktur.py
@@ -0,0 +1,41 @@
+# -*- coding:utf-8 -*-
+"""
+Anmerkung：
+Creator: Xiangyu Jin
+Datum  : 05.04.2022
+"""
+
+"""
+1. Einführung in die Bibliothek
+    * welche 'Werkzeug'
+    * Bsp.: import time, import sys ...
+
+
+2. Das Programm steuert die einzelnen Funktionen des Raspberry Pi(Code-Blöcke)
+    a. Antriebung des Motors: 
+        * Züge fahren und stoppen
+        * wie(Programm), wann(Situation) ... 
+    
+    b. Steuerung der LED-Leuchten und Buzzer: 
+        * Unterschiedliche Antworten für unterschiedliche Situation
+        * wie(Programm), wann(Situation) ...  
+        
+    c. Schaltflächen zur Steuerung der Zugbewegungen:
+        * Eine Person drückt eine Taste, um ein Signal an den Raspberry Pi zu geben
+        * ----> Raspberry Pi steuert Zugbetrieb
+        * wie(Programm) ...  
+        
+    d. Der Bildschirm(LCD) zeigt den Status des Spiels an:
+        * Aktuelle Spielstände auf beiden Seiten
+        * ...        
+        
+    e. Automatische Ermittlung der Ergebnisse:
+        * Wer zuerst im Ziel ist, gewinnt
+        * wie(Programm) ...
+        
+    ......
+    
+    
+3.  Zusammenstellen und Debuggen einzelner Codeblöcke
+
+"""
